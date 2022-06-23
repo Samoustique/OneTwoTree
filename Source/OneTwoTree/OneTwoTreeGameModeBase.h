c13bc9672d80316e9f98d13509dcd39ac62601eb
sphilipps <samuel.philipps@gmail.com>
2022-06-23 20:52:27 +0200

    Create c++ UE project and a Sandbox map

---
+++ Source/OneTwoTree/OneTwoTreeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OneTwoTreeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONETWOTREE_API AOneTwoTreeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
